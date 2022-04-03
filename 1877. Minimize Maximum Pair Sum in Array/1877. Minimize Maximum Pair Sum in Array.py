7c0b2a9d8bc8bb30d1b57491752eddfda9bc7159
birehan <birehananteneh4@gmail.com>
2022-04-03 03:01:58 +0300

    Leet code problem 1877 python solution

---
+++ 1877. Minimize Maximum Pair Sum in Array/1877. Minimize Maximum Pair Sum in Array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minPairSum(self, nums):
+        nums.sort()
+        if len(nums) == 0:
+            return 0
+        l, r = 0, len(nums) - 1
+
+        result = 0
+        while l < r:
+            result = max(result, (nums[l] + nums[r]))
+            l += 1
+            r -= 1
+
+        return result
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
