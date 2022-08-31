0369c57e41c972c1445161694165243a112d2c5e
birehan <91595957+birehan@users.noreply.github.com>
2022-08-31 15:52:18 +0300

    Add files via upload

--- 1838. Frequency of the Most Frequent Element/1838. Frequency of the Most Frequent Element.py
+++ 1838. Frequency of the Most Frequent Element/1838. Frequency of the Most Frequent Element.py
@@ -1,13 +1,16 @@
+from typing import List
+
+
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
         l, r = 0, 0
-        res, total = 0, 0
+        res, total = 0, k
 
         while r < len(nums):
             total += nums[r]
 
-            while nums[r] * (r - l + 1) > total + k:
+            while nums[r] * (r - l + 1) > total:
                 total -= nums[l]
                 l += 1
             res = max(res, r - l + 1)
