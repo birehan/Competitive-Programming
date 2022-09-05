830a156687298faad719068a2f4c8fdbf028a405
birehan <91595957+birehan@users.noreply.github.com>
2022-09-05 10:26:56 +0300

    Add files via upload

--- 56. Merge Intervals/56. Merge Intervals.py
+++ 56. Merge Intervals/56. Merge Intervals.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def merge(self, intervals):
-        result = []
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        for interval in intervals:
-            if result == [] or result[-1][1] < interval[0]:
-                result.append(interval)
+        res = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if res[-1][1] >= intervals[i][0]:
+                res[-1] = [res[-1][0], max(res[-1][-1], intervals[i][-1])]
             else:
-                result[-1][1] = max(result[-1][1], interval[1])
-        return result
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
+                res.append(intervals[i])
+        return res
