de7a681543fce11349570985527eacbc95e5b2c5
birehan <91595957+birehan@users.noreply.github.com>
2022-09-09 15:20:57 +0300

    Add files via upload

--- 621. Task Scheduler/621. Task Scheduler.py
+++ 621. Task Scheduler/621. Task Scheduler.py
@@ -1,15 +1,26 @@
+import heapq
 from collections import Counter
+from typing import List
 
 
-class Solution(object):
-    def leastInterval(self, tasks, n):
-        counter = Counter(tasks)
-        sorted_counter = sorted(counter.values())
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        if n == 0:
+            return len(tasks)
 
-        most_occur = sorted_counter[-1]
-        freq = Counter(sorted_counter)
+        dic = Counter(tasks)
+        maxHeap = [-i for i in dic.values()]
+        heapq.heapify(maxHeap)
 
-        count = freq[most_occur]
-        result = max(((most_occur - 1) * (n + 1) + count), len(tasks))
+        queue = []
+        time = 0
 
-        return result
+        while maxHeap or queue:
+            time += 1
+            if maxHeap:
+                cur_time = 1 + heapq.heappop(maxHeap)
+                if cur_time:
+                    queue.append([cur_time, time + n])
+            if queue and queue[0][1] == time:
+                heapq.heappush(maxHeap, queue.pop(0)[0])
+        return time
