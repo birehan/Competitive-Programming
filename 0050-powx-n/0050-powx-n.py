5cb726e71ddd0b8b3aca607758db1e3690a018ad
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2022-11-14 15:54:07 +0300

    Time: 35 ms (89.60%), Space: 14.3 MB (20.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,16 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
 
-        answer = count = 1
-        cur = x
-        times = abs(n)
-        while times > 0:
-            if count * 2 < times:
-                cur *= cur
-                count *= 2
+        def power(n, count, res, answer):
+            if n == 0:
+                return answer
+            if count * 2 < n:
+                return power(n, count*2, res*res, answer)
             else:
-                answer *= cur
-                cur = x
-                times -= count
-                count = 1
+                return power(n-count, 1, x, answer*res)
+            
+        answer  = power(abs(n), 1, x, 1) 
         return answer if n > 0 else 1/answer
