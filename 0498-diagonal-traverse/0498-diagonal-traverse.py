383bc167b52d067668ade2ba22fb5e849ca7b6c4
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2022-12-29 10:17:09 +0300

    Time: 221 ms (72.46%), Space: 17 MB (99.20%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -2,7 +2,9 @@ class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         answer = []
         row, col = len(mat), len(mat[0])
-        dic = {True:[[-1, 1], [0, 1], [1, 0]],False:[[1, -1], [1, 0], [0, 1]]}
+        dic = {
+            1:[[-1, 1], [0, 1], [1, 0]],
+            0:[[1, -1], [1, 0], [0, 1]]}
 
         r, c = 0, 0
         is_up = True
@@ -10,12 +12,14 @@ class Solution:
         while r < row  or c < col:
             answer.append(mat[r][c])
             
-            for i, j in dic[is_up]:
+            for i, j in dic[int(is_up)]:
                 nr, nc = i+r, j+c
                 if 0 <= nr < row and 0 <= nc < col:
-                    is_up = abs(i)==abs(j) if is_up else (not abs(i)==abs(j))
+                    valid =  abs(i)==abs(j)
+                    is_up = valid if is_up else not valid
                     r, c = nr, nc
                     break
+                    
             else: break
 
         return answer
