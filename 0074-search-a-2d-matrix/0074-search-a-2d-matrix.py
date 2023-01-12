cc57ba677b5f984ba467047c3f4f45c2bd6f202b
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-01-12 06:54:00 +0300

    Time: 39 ms (96.85%), Space: 14.6 MB (7.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,32 +1,18 @@
 class Solution:
-    def findRow(self, target, matrix):
-        top, bottom = 0, len(matrix)
-        while top < bottom:
-            mid = (bottom + top)//2
-            if target > matrix[mid][-1] :
-                top = mid + 1
-            elif target < matrix[mid][0]:
-                bottom = mid - 1
-            else: return mid
-        return top
-    
-    def findCol(self, array, target):
-            left, right = 0, len(array)
-            while left < right:
-                mid = (left + right)//2
-                if target > array[mid] :
-                    left = mid + 1
-                elif target < array[mid]:
-                    right = mid - 1
-                else: return mid
-            return left
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def search(left, right):
+            if left > right:
+                return False
+            middle = (left + right) // 2
+            i, j = middle // n, middle % n
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                return search(middle + 1, right)
+            else:
+                return search(left, middle - 1)
         
-        row = self.findRow(target, matrix)
-        if row >= len(matrix): return False
+       
+        m, n = len(matrix), len(matrix[0])
 
-        col = self.findCol(matrix[row], target)
-        if col >= len(matrix[0]): return False
-
-        return matrix[row][col] == target
+        return search(0, m * n - 1)
