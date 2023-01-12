9ec02e8f0da51271c947622d735387b6f7bfc6da
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-01-12 05:07:30 +0300

    Time: 57 ms (74.58%), Space: 14.1 MB (26.08%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,32 +1,15 @@
 class Solution:
-
-    def helper(self, word):
-            arr = [0] * 26
-            offset = ord("a")
-            for i in word:
-                index = ord(i) - offset
-                arr[index] += 1
-            return arr
-
     def commonChars(self, words: List[str]) -> List[str]:
-        
-        first = self.helper(words[0])
-        offset = ord("a")
-
+        dic = Counter(words[0])
         for i in range(1, len(words)):
-            arr = self.helper(words[i])
-            for i in range(len(arr)):
-                first[i] = min(first[i], arr[i])
+            word = Counter(words[i])
+            for k, v in dic.items():
+                value = min(v, word.get(k, 0))
+                dic[k] = value
         
-        common_chr = []
-
-        for i in range(len(first)):
-            cur = [chr(offset+i)] * first[i]
-            common_chr.extend(cur)
+        answer = []
+        for k, v in dic.items():
+            if v: answer.extend( [k] * v)
         
-        return common_chr
-
-
-
-
+        return answer
        
