01212cded4ef680b4905e558f1c5148071495edf
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-01-20 15:21:27 +0300

    Update Insertion Sort - Part 1.py

--- Insertion Sort - Part 1/Insertion Sort - Part 1.py
+++ Insertion Sort - Part 1/Insertion Sort - Part 1.py
@@ -6,7 +6,6 @@ import random
 import re
 import sys
 
-
 #
 # Complete the 'insertionSort1' function below.
 #
@@ -16,25 +15,17 @@ import sys
 #
 
 def insertionSort1(n, arr):
+    for i in range(n-1):
+        if arr[i] > arr[i+1]:
+            index = i+1 
+            cur = arr[index]
+            while index > 0 and arr[index-1] > cur:
+                arr[index] = arr[index-1] 
+                print(*arr)
+                index -= 1
+            arr[index] = cur
+            print(*arr)
     # Write your code here
-    for i in range(1, n):
-        if arr[i] < arr[i - 1]:
-            unsorted = arr[i]
-
-            for j in range(i, 0, -1):
-                if (unsorted < arr[j - 1]):
-                    arr[j] = arr[j - 1]
-                    print(*arr)
-
-                else:
-                    arr[j] = unsorted
-                    print(*arr)
-                    break
-                if ((unsorted < arr[j - 1]) and (j - 1 == 0)):
-                    arr[j - 1] = unsorted
-                    print(*arr)
-    return
-
 
 if __name__ == '__main__':
     n = int(input().strip())
