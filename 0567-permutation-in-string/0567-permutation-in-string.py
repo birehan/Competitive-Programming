46a2b45f7ae9db1850702f8b953d820fa8665b34
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-02-23 17:11:36 +0300

    Time: 77 ms (67.67%), Space: 13.9 MB (93.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,15 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        dic = Counter(s1)
-        compare = Counter()
+        count1 = Counter(s1)
+        count2 = defaultdict(int)
         left = 0
         for right, string in enumerate(s2):
-            compare[string] += 1
-            if compare == dic:
+            count2[string] += 1
+            if count1 == count2:
                 return True
-            if right-left+1 == len(s1):
-                compare[s2[left]] -= 1
+            if right - left + 1 == len(s1):
+                count2[s2[left]] -= 1
+                if not count2[s2[left]]:
+                    del count2[s2[left]]
                 left += 1
-
+        
         return False
-    
