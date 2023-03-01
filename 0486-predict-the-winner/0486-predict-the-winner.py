37eac55daaf9d949ba00dd4bb9037f587d637793
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-03-01 11:15:50 +0300

    Time: 5886 ms (9.94%), Space: 13.8 MB (79.38%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,12 +1,26 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        dp = {}
+        # choosing the optimal every time
+        # player 1 is positive and player 2 negative
+        # return -v
 
-        def find(i, j):
-            if (i, j) not in dp:
-                if i == j:
-                    return nums[i]
-                dp[i,j] = max(nums[i]-find(i+1, j), nums[j]-find(i, j-1))
-            return dp[i,j]
+        def helper(left, right, turn):
+            if left == right:
+                return turn * nums[left]
 
-        return find(0, len(nums)-1) >= 0
+            left_value = nums[left] * turn
+            left_value += helper(left+1, right, turn * -1)
+
+            right_value = nums[right] * turn
+            right_value += helper(left, right-1, turn * -1)
+
+            result = max(left_value, right_value) if turn == 1 else min(left_value, right_value)
+
+            return result
+            
+        result = helper(0, len(nums)-1, 1)
+    
+        return result >= 0
+
+
+
