6d22459b0ddf6839246b984fd459c2d5d51ea631
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-03-15 22:27:31 +0300

    Time: 42 ms (10.92%), Space: 13.8 MB (98.28%) - LeetHub

--- 0306-additive-number/0306-additive-number.py
+++ 0306-additive-number/0306-additive-number.py
@@ -1,38 +1,23 @@
 class Solution:
-    def validate(self, values):
-        if int(values[0]) + int(values[1]) != int(values[2]):
-            return False
-        for value in values:
-            if len(value) != len(str(int(value))):
-                return False
-        return True
-        
-    def helper(self, num1, num2, index):
-        if index == len(self.num):
-            return
-        
-        cur = ""
-        for i in range(index, len(self.num)):
-            cur += self.num[i]
-
-            if not num1:
-                self.helper((cur), num2, i+1)
-
-            elif not num2:
-                self.helper(num1, (cur), i+1)
-                
-            elif self.validate([num1, num2, cur]):
-                if i+1 == len(self.num):                   
-                    self.answer = True
-                self.helper(num2, (cur), i+1)
-
-            elif int(cur) > int(num1) + int(num2):
-                return 
-            
-
-            
     def isAdditiveNumber(self, num: str) -> bool:
-        self.answer = False
-        self.num = num
-        self.helper("", "", 0)
-        return self.answer 
+        
+        self.isValid = False
+        def backtrack(num,arr):
+            
+            if num=="":
+                if not self.isValid and len(arr)>2:
+                    self.isValid = True
+                    
+            candidates = [i for i in range(1,len(num)+1)]
+
+            for candidate in candidates:
+                intVal = num[:candidate]
+                if (len(intVal)==len(str(int(intVal)))) and (len(arr)<2 or arr[-1]+arr[-2] == int(intVal)):
+                    arr.append(int(intVal))
+                    backtrack(num[candidate:],arr)
+                    arr.pop()
+        
+        
+        
+        backtrack(num,[])
+        return self.isValid
