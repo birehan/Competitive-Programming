628478f6cd36e8dd68f64db024e43003d6aa5fba
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-03-22 15:37:50 +0300

    Time: 34 ms (82.82%), Space: 13.8 MB (98.81%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,30 @@
+class Solution:
+  def helper(self, cur, index, s):
+    if index == len(s) and len(cur) == 4:
+      self.answer.append(".".join(cur.copy()))
+    
+    if len(cur) >= 4:
+      return
+    
+    num  = ""
+    for i in range(index, index+3):
+      if i >= len(s):
+        break
+      
+      num += s[i]
+
+      if (len(num) > 1 and num[0] == '0') or int(num) > 255:
+        break
+
+      cur.append(num)
+      self.helper(cur, i + 1, s)
+      cur.pop()
+
+
+  def restoreIpAddresses(self, s: str) -> List[str]:
+    if len(s) < 4 or len(s) > 12:
+      return []
+      
+    self.answer = []
+    self.helper([], 0, s)
+    return self.answer
