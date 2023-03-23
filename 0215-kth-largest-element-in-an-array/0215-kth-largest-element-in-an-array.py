fcd0a3e82b710e7a2cbb4d260d17742a5b0403ce
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-03-23 19:00:14 +0300

    Time: 665 ms (31.22%), Space: 27.1 MB (57.13%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,6 +1,33 @@
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        res = heapq.nlargest(k, nums)
-        return res[-1]
+    def helper(self, left, right):
+        pivot = randint(left, right-1)
+        self.nums[pivot], self.nums[left] =  self.nums[left], self.nums[pivot]
         
-     
+        pivot = left
+        write = left+1
+
+        for read in range(left+1, right):
+            if self.nums[read] < self.nums[pivot]:
+                self.nums[read], self.nums[write] =  self.nums[write], self.nums[read]
+                write += 1
+
+        self.nums[write-1], self.nums[pivot] =  self.nums[pivot], self.nums[write - 1]
+        
+        if len(self.nums) - self.k  == write-1:
+            return
+        elif len(self.nums) - self.k > write-1:
+            self.helper(write, right)
+        else:
+            self.helper(left, write-1)
+
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        self.nums = nums
+        self.k = k
+        self.helper(0, len(nums))
+
+        return self.nums[-k]
+        # quick select until you find pivot_index == n-k
+
+   
+
