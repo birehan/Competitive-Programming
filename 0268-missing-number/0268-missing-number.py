d576e2e7dcaa1268bc26a1de658df5614e7f3504
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-03-24 17:36:32 +0300

    Time: 164 ms (26.34%), Space: 15.2 MB (67.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,4 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-       total = (len(nums) * (len(nums)+1))//2
-       return total - sum(nums)
+        index = 0
+
+        while index < len(nums):
+            connect = nums[index]
+            if connect != index and connect < len(nums):
+                nums[connect], nums[index] = nums[index], nums[connect]
+            else:
+                index += 1
+        
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        
+        return len(nums)
