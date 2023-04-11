bc98bd756a8d65e1a094c3a4672fecb9716841c7
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-04-11 18:51:10 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,14 +1,17 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        factors = 0
-        d = 2
-        while d * d <= n:
-            while n % d == 0:
-                factors += d
-                n //= d
-            d += 1
+        cache = {}
+        def helper(screen, clipboard):
+            if (screen, clipboard) in cache: return cache[(screen, clipboard)]
+            if screen == n: return 0
+            if screen > n: return float("Inf")
+            
+            copy_paste = helper(screen+screen, screen) + 2
+            paste = float("Inf")
+            if clipboard:
+                paste = helper(screen + clipboard, clipboard) + 1
+
+            cache[(screen, clipboard)] = min(copy_paste, paste)    
+            return cache[(screen, clipboard)]
         
-        if n > 1:
-            factors += n
-        
-        return factors
+        return helper(1, 0)
