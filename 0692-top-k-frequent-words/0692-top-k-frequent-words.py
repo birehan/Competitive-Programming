1406ac8693a56244970a820d3fd9af3498f38d80
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-05-05 21:11:11 +0300

    Time: 77 ms (6.78%), Space: 16.3 MB (7.47%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,7 +1,26 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         counter = Counter(words)
-        sortdict = sorted(counter.items(), key=lambda x:[-x[1], x[0]], reverse=True)
-        answer = [sortdict[i][0] for i in range(-1, -k-1, -1)]
+        pair = defaultdict(list)
 
+        for key, freq in counter.items():
+            pair[-freq].append(key)
+        
+        pair = list(pair.items())
+        heapify(pair)
+        answer = []
+
+        while pair and len(answer) < k:
+            freq, items = heappop(pair)
+            heapify(items)
+            
+            while len(answer) < k and items:
+                answer.append(heappop(items))
+        
         return answer
+
+
+        
+
+        
+
