26259343811fbb6de7bedeefd34e35e93c0eca7f
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-05-19 10:00:05 +0300

    Time: 114 ms (52.41%), Space: 21.4 MB (5.31%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,44 +5,22 @@
 #         self.next = nextListNodet
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-
-        def merge(list1, list2):
-            if not list1 or not list2:
-                return list1 or list2
-            prev = head = None
-            temp = list1
-            while temp and list2:
-                if list2.val < temp.val:
-                    if not prev:
-                        cur = list2
-                        list2 = list2.next
-                        cur.next = temp
-                        prev, temp = cur, cur
-                        temp = temp.next
-                    else:
-                        cur = temp
-                        prev.next = list2
-                        list2 = list2.next
-                        prev = prev.next
-                        temp = prev
-                        temp.next = cur
-                        temp = temp.next
-                else:
-                    prev = temp
-                    temp = temp.next
-                if prev and not head:
-                    head = prev
-            if list2:
-                prev.next = list2
-
-            return head
-       
+        heap = []
         if not lists:
             return None
+        
+        for item in lists:
+            while item:
+                heappush(heap, item.val)
+                item = item.next
+        
+        head = ListNode(0)
+        temp = head
+        while heap:
+            temp.next = ListNode(heappop(heap))
+            temp = temp.next
 
-        head = lists[0]
-        for i in range(1, len(lists)):
-            head = merge(head, lists[i])
+        return head.next
+
+
             
-        return head
-      
