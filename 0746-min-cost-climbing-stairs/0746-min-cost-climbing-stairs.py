5e9d6a11fb0ea56b789dc3b379daaed42c202f9b
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-05-28 15:55:35 +0300

    Time: 77 ms (19.19%), Space: 17.4 MB (8.93%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,12 +1,21 @@
 class Solution:
+    def helper(self, n, dp):
+        if n in  [0, 1]:
+            return self.cost[n]
+        
+        if n not in dp:
+            dp[n] = self.cost[n] + min(self.helper(n-1, dp), self.helper(n-2, dp))
+        return dp[n]
+
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        self.cost = [0] + cost + [0]
+        dp = defaultdict(int)
         
-        cost.append(0)
+        self.helper(len(self.cost)-1, dp)
+        return dp[len(self.cost)-1]
+
         
-        for i in range(len(cost)-3, -1, -1):
-            cost[i] += min(cost[i+1], cost[i+2]) 
-        
-        return min(cost[0], cost[1])
+      
             
         
             
