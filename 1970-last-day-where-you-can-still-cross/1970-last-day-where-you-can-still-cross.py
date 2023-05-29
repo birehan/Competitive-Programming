5b9e7401eab1c4d3dd150e7b67a3ba6056f4c04f
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-05-29 15:01:24 +0300

    Time: 1638 ms (84.88%), Space: 30.2 MB (18.60%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
@@ -1,51 +1,35 @@
 class Solution:
+    def helper(self, mainRoot, subRoot,a , b):
+        if self.answer[mainRoot] == []:
+            self.answer[mainRoot] = [False, False]
+
+        if self.answer[subRoot] != []:
+            self.answer[mainRoot][0] = self.answer[mainRoot][0] or self.answer[subRoot][0]
+            self.answer[mainRoot][1] = self.answer[mainRoot][1] or self.answer[subRoot][1]
+
+        
+        if a[1] == 1 or b[1] == 1:
+            self.answer[mainRoot][0] = True
+        
+        if a[1] == self.col  or b[1] == self.col:
+            self.answer[mainRoot][1] = True
+        
+        if self.answer[mainRoot][0] and self.answer[mainRoot][1]:
+            return False
+        
+        return True
+
     def union(self, a, b):
         rootA, rootB = self.find(a), self.find(b)
         if rootA != rootB:
-               
-
             if self.rank[rootA] >= self.rank[rootB]:
-             
                 self.rank[rootA] += self.rank[rootB]
                 self.root[rootB] = self.root[rootA]
-
-                if self.answer[rootA] == []:
-                    self.answer[rootA] = [False, False]
-                
-                if self.answer[rootB] != []:
-                    self.answer[rootA][0] = self.answer[rootB][0] or self.answer[rootA][0]
-                    self.answer[rootA][1] = self.answer[rootB][1] or self.answer[rootA][1]
-
-                if a[1] == 1 or b[1] == 1:
-                    self.answer[rootA][0] = True
-                
-                if a[1] == self.col  or b[1] == self.col :
-                    self.answer[rootA][1] = True
-
-                if self.answer[rootA][0] and self.answer[rootA][1]:
-                    return False
-                
-
+                return self.helper(rootA, rootB, a, b)
             else:
                 self.rank[rootB] += self.rank[rootA]
                 self.root[rootA] = self.root[rootB]
-
-                if self.answer[rootB] == []:
-                    self.answer[rootB] = [False, False]
-
-                if self.answer[rootA] != []:
-                    self.answer[rootB][0] = self.answer[rootB][0] or self.answer[rootA][0]
-                    self.answer[rootB][1] = self.answer[rootB][1] or self.answer[rootA][1]
-
-                
-                if a[1] == 1 or b[1] == 1:
-                    self.answer[rootB][0] = True
-                
-                if a[1] == self.col  or b[1] == self.col:
-                    self.answer[rootB][1] = True
-                
-                if self.answer[rootB][0] and self.answer[rootB][1]:
-                    return False
+                return self.helper(rootB, rootA, a, b)
             
         return True
 
@@ -63,6 +47,7 @@ class Solution:
         watered_cells = set()
 
         dirs = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [-1, 1], [1, -1]]
+        
         time = 0
         for r, c in cells:
             self.root[(r, c)] = (r, c)
