4d8c03f38cea91c37068e63a28d67a3e97df11c7
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-05-31 09:10:12 +0300

    Time: 44 ms (77.42%), Space: 16.3 MB (42.37%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,25 +1,17 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort()
-
-        def compute_sum(index):
-            start = 1
-            summ = 0
-            for i in range(index, len(satisfaction)):
-                summ += start * satisfaction[i]
-                start += 1
-            
-            return summ
         
-        zero_ind = bisect_left(satisfaction, 0)
-        max_sum = compute_sum(zero_ind)
-
-        while zero_ind:
-            zero_ind -= 1
-            cur_sum = compute_sum(zero_ind)
+        max_sum = 0
+        value_sum = cur_sum = 0
+        
+        for i in range(len(satisfaction)-1, -1, -1):
+            cur_sum += satisfaction[i] + value_sum
+            
             if cur_sum <= max_sum:
                 return max_sum
-
+            
+            value_sum += satisfaction[i]
             max_sum = cur_sum
             
         
