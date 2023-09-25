1351a2de3af08fe5391989258c4394f5a6ee6ad3
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-09-25 17:33:21 +0300

    Time: 594 ms (13.03%), Space: 189.1 MB (6.27%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -2,21 +2,15 @@ class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort()
         
-        max_sum = 0
-        value_sum = cur_sum = 0
-        
-        for i in range(len(satisfaction)-1, -1, -1):
-            cur_sum += satisfaction[i] + value_sum
+        @cache
+        def dfs(index, multi):
+            if index >= len(satisfaction):
+                return 0
             
-            if cur_sum <= max_sum:
-                return max_sum
-            
-            value_sum += satisfaction[i]
-            max_sum = cur_sum
-            
-        
-        return max_sum
+            a = (multi * satisfaction[index]) + dfs(index + 1, multi + 1)
+            b = dfs(index + 1, multi)
 
-            
-
+            return max(a, b)
+        
+        return dfs(0, 1)
         
