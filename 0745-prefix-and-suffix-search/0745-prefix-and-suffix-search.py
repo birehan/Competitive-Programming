61b7931fd3192100edbd40fa37b5dcef6e548fe8
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-02 11:52:21 +0300

    Time: 1526 ms (67.73%), Space: 55.3 MB (66.93%) - LeetHub

--- 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.py
+++ 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.py
@@ -7,41 +7,33 @@ class TrieNode:
 class WordFilter:
     def __init__(self, words: List[str]):
         self.root = TrieNode()
-        for i in range(len(words)):
-            self.insert(words[i], i)
-    
-    def insert(self, word, index):
-        cur = self.root
-        n = len(word)
-        for i in range(n):
-            pre = word[i]
-            post = word[n-i-1]
-            if (pre, post) not in cur.children:
-                cur.children[(pre, post)] = TrieNode()
-            
-            cur.children[(pre, post)].indexes.append(index)
-        
-            cur = cur.children[(pre, post)]
-        
+        for index, word in enumerate(words):
+            cur = self.root
+            n = len(word)
+
+            for i in range(n):
+                if (word[i], word[n-i-1]) not in cur.children:
+                    cur.children[(word[i], word[n-i-1])] = TrieNode()
+                
+                cur.children[(word[i], word[n-i-1])].indexes.append(index)
+                cur = cur.children[(word[i], word[n-i-1])]
 
     def f(self, pref: str, suff: str) -> int:
         pre_index, suf_index = 0, len(suff)-1
-        cur = self.root
-        while pre_index < len(pref) and suf_index >= 0:
-            if (pref[pre_index], suff[suf_index]) not in cur.children:
-                return -1
+        queue = deque([self.root])
+
+        while pre_index < len(pref) or suf_index >= 0:
+
+            if pre_index < len(pref) and suf_index >= 0:
+                if (pref[pre_index], suff[suf_index]) not in queue[0].children:
+                    return -1
+                
+                queue[0] = queue[0].children[(pref[pre_index], suff[suf_index])]
+
+                pre_index += 1
+                suf_index -= 1
             
-            cur = cur.children[(pref[pre_index], suff[suf_index])]
-
-            pre_index += 1
-            suf_index -= 1
-        
-    
-        if pre_index < len(pref):
-            queue = deque([cur])
-
-            while queue and pre_index < len(pref):
-
+            elif pre_index < len(pref):
                 for _ in range(len(queue)):
                     cur = queue.popleft()
                     for a, b  in cur.children:
@@ -49,18 +41,8 @@ class WordFilter:
                             queue.append(cur.children[(a, b)])
                     
                 pre_index += 1
-            
-            if queue:
-                return max([cur.indexes[-1] for cur in queue])
-            
-            else:
-                return -1
-        
-        if suf_index >= 0:
-            queue = deque([cur])
 
-            while queue and suf_index >=0:
-
+            elif suf_index >= 0:
                 for _ in range(len(queue)):
                     cur = queue.popleft()
                     for a, b in  cur.children:
@@ -68,16 +50,15 @@ class WordFilter:
                             queue.append(cur.children[(a, b)])
                     
                 suf_index -= 1
-            
-            if queue:
-                return max([cur.indexes[-1] for cur in queue])
-            else:
-                return -1
-        
+                          
 
-        return cur.indexes[-1]
+        if queue:
+            return max([cur.indexes[-1] for cur in queue])
+        
+        return -1
 
         
+
 
 
 # Your WordFilter object will be instantiated and called as such:
