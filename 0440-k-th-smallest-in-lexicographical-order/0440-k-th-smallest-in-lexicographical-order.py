487adc59e8820026fbea2ce96d7fae9037caf64a
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-04 10:35:00 +0300

    Time: 35 ms (78.76%), Space: 16.3 MB (57.52%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -1,36 +1,24 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        if k == 1:
-            return 1
-
         length = len(str(n))
         answer = []
-        count = 100
-        while k > 0 and count:
-            count -= 1
+        while k > 1:
 
             start = 1 if not answer else 0
             cur_nth = 0
+            prev = "".join(answer)
 
-            if answer and k == 1:
-                return int("".join(answer))
-
-            if answer and k != 1:
+            if answer:
                 k -= 1
 
-
             for num in range(start, 10):
-                
                 num_count = 0
-                prev = "".join(answer)
                 for nth in range(length - len(answer)):
-                    top_value = int(prev + str(num + 1) + str("0" * nth))
+                    top_value = int(prev + str(num + 1) + ("0" * nth))
                     if top_value <= n:
-                        num_count += int(str(1) + str("0") * nth)
+                        num_count += int(str(1) + ("0" * nth))
                     else:
-                        val = (n - int(prev + str(num) + str("0" * nth)))
-                        if val >= 0:
-                            num_count += (val + 1)
+                        num_count += max(-1, (n - int(prev + str(num) + ("0" * nth)))) + 1
 
                 if cur_nth + num_count >= k:
                     answer.append(str(num))
@@ -39,5 +27,4 @@ class Solution:
                 else:
                     cur_nth += num_count
 
-        return int("".join(answer))
-        # 
+        return int("".join(answer) if answer else 1)
