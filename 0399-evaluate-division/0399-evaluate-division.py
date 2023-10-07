667863da066ad59bf2ba47cbfafa89b43e07b57b
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-07 23:36:19 +0300

    Time: 37 ms (75.09%), Space: 16.3 MB (84.32%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,23 +1,39 @@
-from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = defaultdict(dict)
-        for eq,val in zip(equations,values):
-            graph[eq[0]][eq[1]]=val
-            graph[eq[1]][eq[0]]=1/val
-        
-        return [calculate(q[0],q[1],graph,1.0,set()) for q in queries]
+        graph = defaultdict(list)
+        for i in range(len(equations)):
+            graph[equations[i][0]].append((equations[i][1], values[i]))
+            graph[equations[i][1]].append((equations[i][0], 1/values[i]))
         
 
 
+        answer = []
+        for a, b in queries:
+                
+            visited = set([a])
+            queue = deque([(a, 1)])
+            ans = -1
 
-def calculate(dividend,divisor,graph,k,visited):
-    if divisor in graph[dividend]:
-        return k*graph[dividend][divisor]
-    for neighbour in graph[dividend]:
-        if neighbour not in visited:
-            visited.add(neighbour)
-            result = calculate(neighbour,divisor,graph,k*graph[dividend][neighbour],visited)
-            if result != -1.0:
-                return result
-    return -1.0
+            while queue:
+                node, val = queue.popleft()
+
+                if node == b:
+                    if a not in graph:
+                        ans = -1
+                    else:
+                        ans = val
+                    break
+                
+                if node in graph:
+                    for child,edge_val in graph[node]:
+                        if child not in visited:
+                            queue.append((child, val * edge_val))
+                            visited.add(child)
+            
+
+            answer.append(ans)
+
+
+
+        
+        return answer
