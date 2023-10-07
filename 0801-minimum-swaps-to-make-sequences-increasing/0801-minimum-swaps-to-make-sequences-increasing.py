e037702d2b4569aa7729329778165570b4f2cbe5
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-07 23:03:53 +0300

    Time: 1219 ms (33.95%), Space: 34.1 MB (67.90%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
@@ -1,22 +1,22 @@
 class Solution:
     def minSwap(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
-        start = {}
+        start = defaultdict(lambda:inf)
         a, b = nums1[0], nums2[0]
-        start[(a, b)] = min( start.get((a, b), inf), 0)
-        start[(b, a)] = min(start.get((b, a), inf), 1)
+        start[(a, b)] = min(start[(a, b)] , 0)
+        start[(b, a)] = min(start[(b, a)], 1)
         
 
         for i in range(1, n):
             a, b = nums1[i], nums2[i]
-            new_start = {}
+            new_start = defaultdict(lambda:inf)
             for c, d in start.keys():
                 cost = start[(c, d)]
                 if a > c and b > d:
-                    new_start[(a, b)] = min(new_start.get((a, b), inf), cost)
+                    new_start[(a, b)] = min(new_start[(a, b)], cost)
                 
                 if a > d and b > c:
-                    new_start[(b, a)] = min(new_start.get((b, a), inf), cost + 1)
+                    new_start[(b, a)] = min(new_start[(b, a)], cost + 1)
             
             start = new_start
 
