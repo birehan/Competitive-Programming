b0ec9175f837025808137f4125fd050b22513e1f
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-08 11:01:07 +0300

    Time: 49 ms (56.25%), Space: 17.2 MB (74.09%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -6,25 +6,33 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_width = 1
         if not root:
             return 0
         
-        queue = deque([[root, 1]])
+        max_width = 1
+        queue = deque([(root, 1)])
+
         while queue:
-            cur_width = queue[-1][1] - queue[0][1] + 1
-            max_width = max(max_width, cur_width)
+            min_val, max_val = inf, -inf
+            n = len(queue)
 
-            if cur_width == 1:
-                queue[0][1] = 1
+            for _ in range(n):
+                node, position = queue.popleft()
 
-            for _ in range(len(queue)):
-                cur = queue.popleft()
+                min_val = min(min_val, position)
+                max_val = max(max_val, position)
+
+                if node.left:
+                    queue.append((node.left, position * 2 - 1))
                 
-                if cur[0].left:
-                    queue.append([cur[0].left, cur[1]*2 - 1])
-                
-                if cur[0].right:
-                    queue.append([cur[0].right, cur[1]*2])
+                if node.right:
+                    queue.append((node.right, position * 2))
+            
+            max_width = max(max_width, max_val - min_val + 1)
         
         return max_width
+
+
+            
+
+        
