ba273f0fa91f835a93f7e0f4d3c207e2f1d5c8fc
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-10 22:07:38 +0300

    Time: 40 ms (45.72%), Space: 16.2 MB (51.80%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,6 +1,34 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle in haystack:
-            return haystack.index(needle)
+        mod = 10 ** 9 + 7
+        alpha = 27
+        powers = []
+        n = len(needle)
+
+        for i in range(n+1):
+            powers.append((alpha ** i) % mod)
+        
+        needle_key = 0
+        m = len(haystack)
+
+        for i in range(n):
+            
+            needle_key += ((powers[n-i-1] % mod * (ord(needle[i]) - 96) % mod) % mod)
+            needle_key %= mod
+        
+        left = 0
+        value = 0
+        for right in range(m):
+            value = ((value * alpha) + ((ord(haystack[right]) - 96)))% mod
+
+            if right - left + 1 == n:
+                if value == needle_key:
+                    return left
+                
+                value = (value - ((ord(haystack[left]) - 96) * powers[right-left])) % mod
+
+                left += 1
+        
         return -1
-     
+
+            
