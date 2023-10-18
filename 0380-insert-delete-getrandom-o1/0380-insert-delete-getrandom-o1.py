271bf57f4126306a3ebba4260b69daa3f9392165
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-18 11:07:56 +0300

    Time: 299 ms (92.28%), Space: 63.6 MB (60.13%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,20 +1,33 @@
+import random
+
 class RandomizedSet:
     def __init__(self):
-        self.store = set()
-        
+        self.val_to_index = {}
+        self.values = []
+
     def insert(self, val: int) -> bool:
-        if val in self.store:
+        if val in self.val_to_index:
             return False
-        
-        self.store.add(val)
-        return True 
+
+        self.val_to_index[val] = len(self.values)
+        self.values.append(val)
+        return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.store:
+        if val not in self.val_to_index:
             return False
+
+        # Swap the value to remove with the last value in the list
+        index_to_remove = self.val_to_index[val]
+        last_value = self.values[-1]
+        self.values[index_to_remove] = last_value
+        self.val_to_index[last_value] = index_to_remove
+
+        # Delete the value and its index
+        del self.val_to_index[val]
+        self.values.pop()
         
-        self.store.remove(val)
         return True
 
     def getRandom(self) -> int:
-        return random.choice(tuple(self.store))
+        return random.choice(self.values)
