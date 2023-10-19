63d3b6113d77e0ac985399b1b04556e0fae09b86
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-19 10:51:58 +0300

    Time: 777 ms (58.45%), Space: 233.8 MB (8.44%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        summ = sum(nums)
+
+        if summ % 2 != 0:
+            return False
+        
+        nums.sort()
+
+        @cache
+        def dfs(total, index):
+            if index >= len(nums):
+                return False
+
+            if total == summ//2:
+                return True
+            
+            if dfs(total + nums[index], index + 1):
+                return True
+            
+            if dfs(total, index + 1):
+                return True
+        
+        return dfs(0, 0)
+
+        
