8d4a64b510ca6defb666decc40ae0cb73cead37e
Birehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-10-19 10:18:56 +0300

    Time: 48 ms (48.64%), Space: 16.4 MB (27.30%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,32 +1,31 @@
 class Solution:
     def union(self, a, b):
         rootA, rootB = self.find(a), self.find(b)
-       
-        if rootA <= rootB:
-            self.root[rootB] = self.root[rootA]
-        else:
-            self.root[rootA] = self.root[rootB]
+        if rootA == rootB:
+            return
         
+        if rootA < rootB:
+            self.root[rootB] = rootA
+        else:
+            self.root[rootA] = rootB
+    
     def find(self, a):
+        if a not in self.root:
+            self.root[a] = a
+
         if self.root[a] != a:
             self.root[a] = self.find(self.root[a])
-
         return self.root[a]
 
-
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        self.root = dict([(chr(i+97), chr(i+97)) for i in range(26)])
-        
+        self.root = {}
+
         for i in range(len(s1)):
             self.union(s1[i], s2[i])
-        
-        answer = []
-        for b in baseStr:
-            answer.append(self.find(b))
-        
-        return "".join(answer)
-        
 
+        ans = []
+        for s in baseStr:
+            ans.append(self.find(s))
         
-
+        return "".join(ans)
         
